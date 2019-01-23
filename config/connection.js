0c32778c62fe51e33564585af30ebac77ab9ee03
Ubadah Atta <ubadahatta@Ubadahs-MacBook.local>
2019-01-23 14:14:54 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-const connection = mysql.createConnection({
+
+ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
 });
-
+};
 connection.connect(function(err){
     if (err){
         console.log(`error connecting: ${err.stack}`);
@@ -15,5 +20,7 @@ connection.connect(function(err){
     }
     console.log(`connected as id ${connection.threadId}`);
 });
+
+
 
 module.exports = connection;
